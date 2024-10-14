2a8a3e4825d88fef7f50799c2797e3c2f1509b4d
Dennis Chan <dennisming2012@gmail.com>
2024-10-14 22:31:45 +0800

    refactor: display info

--- scriptable.mjs
+++ scriptable.mjs
@@ -3,14 +3,16 @@ const headers = {
   'Content-Type': 'application/json',
 };
 const FS_BUS_STOP_ID = 'K17-D010';
-
-const req = new Request(url);
-req.method = 'POST';
-req.headers = headers;
-req.body = JSON.stringify({
+const METHOD = 'POST';
+const BODY = {
   language: 'zh',
   routeName: 'K17',
-});
+};
+
+const req = new Request(url);
+req.method = METHOD;
+req.headers = headers;
+req.body = JSON.stringify(BODY);
 
 const data = await req.loadJSON();
 const alert = new Alert();
@@ -19,9 +21,11 @@ if (data.routeStatusRemarkContent === '停止服務') {
   alert.title = data.routeStatusRemarkContent;
   alert.message = data.footerRemarks;
 } else {
-  const [bus] = data.busStop.find((bs) => bs.busStopId === FS_BUS_STOP_ID).bus;
-  alert.title = 'K17';
-  alert.message = bus.departureTimeText;
+  const buses = data.busStop.find((bs) => bs.busStopId === FS_BUS_STOP_ID).bus;
+  const firstBus = buses[0];
+  const secondBus = buses[1];
+  alert.title = '下班車仲有幾耐走';
+  alert.message = `1) ${firstBus.departureTimeText}\n2) ${secondBus.departureTimeText}`;
 }
 
 alert.presentAlert();
