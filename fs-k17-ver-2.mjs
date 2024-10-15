0a6e79341fce65b6387f530851d942ff66aef1f6
Dennis Chan <dennisming2012@gmail.com>
2024-10-15 22:11:27 +0800

    feat: add ver 2

---
+++ fs-k17-ver-2.mjs
@@ -0,0 +1,35 @@
+const url = 'https://rt.data.gov.hk/v1/transport/mtr/bus/getSchedule';
+const headers = {
+  'Content-Type': 'application/json',
+};
+const FS_BUS_STOP_ID = 'K17-D010';
+const METHOD = 'POST';
+const BODY = {
+  language: 'zh',
+  routeName: 'K17',
+};
+
+const req = new Request(url);
+req.method = METHOD;
+req.headers = headers;
+req.body = JSON.stringify(BODY);
+
+let response = await fetch(url, {
+  method: METHOD,
+  headers: headers,
+  body: JSON.stringify(BODY),
+});
+
+const data = await req.loadJSON();
+let result = '';
+
+if (data.routeStatusRemarkContent === '停止服務') {
+  result = `${data.routeStatusRemarkContent}\n${data.footerRemarks}`;
+} else {
+  const buses = data.busStop.find((bs) => bs.busStopId === FS_BUS_STOP_ID).bus;
+  const firstBus = buses[0];
+  const secondBus = buses[1];
+  result = `下班車仲有幾耐走\n1) ${firstBus.departureTimeText}\n2) ${secondBus.departureTimeText}`;
+}
+
+return result;
