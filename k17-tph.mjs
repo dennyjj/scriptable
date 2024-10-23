c4c624aa706f6fc6d218ef4a04c16b145af22b3d
Dennis Chan <dennisming2012@gmail.com>
2024-10-23 22:17:56 +0800

    feat: add tph k17 script

---
+++ k17-tph.mjs
@@ -0,0 +1,42 @@
+//todo: parameterize bus stop id and route name
+
+// default config
+const url = 'https://rt.data.gov.hk/v1/transport/mtr/bus/getSchedule';
+const headers = {
+  'Content-Type': 'application/json',
+};
+let TPH_BUS_STOP_ID = 'K17-U010';
+const METHOD = 'POST';
+const BODY = {
+  language: 'zh',
+  routeName: 'K17',
+};
+
+// send request to server
+const req = new Request(url);
+req.method = METHOD;
+req.headers = headers;
+req.body = JSON.stringify(BODY);
+const data = await req.loadJSON();
+
+// compose notification and schedule to user
+let title = '';
+let message = '';
+if (!data.busTop.length) {
+  title = 'K17停止服務';
+  message = `${data.routeStatusRemarkContent}\n${data.footerRemarks}`;
+} else {
+  const buses = data.busStop.find((bs) => bs.busStopId === TPH_BUS_STOP_ID).bus;
+  const firstBus = buses[0];
+  const secondBus = buses[1];
+  title = 'K17下班車仲有幾耐走';
+  message = `1) ${firstBus.departureTimeText}\n2) ${secondBus.departureTimeText}`;
+}
+
+const noti = new Notification();
+noti.title = title;
+noti.body = message;
+noti.schedule();
+
+// complete script
+Script.complete();
